3160c3f7d71ead1d760029a6c6baf44b75916949
Abdoul <abdoulramanediallo44@gmail.com>
2023-05-31 18:24:06 +0000

    new Update

--- script.js
+++ script.js
@@ -11,15 +11,9 @@ class Book {
 
     container.innerHTML = '';
 
-    if (mybooks.length === 0) {
-      container.style.border = 'none';
-    } else {
-      container.style.border = '2px black solid';
-    }
-
     const bookCards = mybooks.map((b) => `
       <ul class="book-ul">
-        <li>
+        <li class="book-li">
           <h3>"${b.title}"
           by
           ${b.author}</h3>
@@ -27,7 +21,21 @@ class Book {
         </li>
       </ul>
     `);
-    container.innerHTML = bookCards.join('');
+
+    if (mybooks.length === 0) {
+      container.style.background = 'none';
+      container.innerHTML = `<p>There is no books yet</p>
+      <p>
+      go to Add new tab and add a book !
+      <a class="nav-link" id="addNew"
+          onclick='Book.showIt("add-section");  Book.toggleActive("addNew");' >Add new</a>
+      </p>
+      
+      `;
+    } else {
+      container.style.background = '#f8f1f1';
+      container.innerHTML = bookCards.join('');
+    }
   }
 
   static addNew(e) {
@@ -60,9 +68,65 @@ class Book {
     localStorage.setItem('addBooks', JSON.stringify(addBooks));
     Book.showBooks();
   }
+
+  static showIt(id) {
+    const sectionList = ['list-section', 'add-section', 'about-section'];
+
+    sectionList.forEach((e) => {
+      if (e === id) {
+        document.querySelector(`#${e}`).setAttribute('data-visible', 'true');
+      } else {
+        document.querySelector(`#${e}`).setAttribute('data-visible', 'false');
+      }
+    });
+  }
+
+  static toggleActive(id) {
+    const links = document.querySelectorAll('.nav-link');
+    links.forEach((link) => {
+      link.classList.remove('active');
+    });
+    const clickedLink = document.getElementById(id);
+    clickedLink.classList.add('active');
+  }
+
+  static showNaviagtionBar() {
+    const navigation = document.querySelector('#navigation-container');
+
+    navigation.innerHTML = `<nav>
+    <h5>Awesome Books</h5>
+      <ul>
+          <li class="nav-link active" id="list" 
+          onclick='Book.showIt("list-section");  Book.toggleActive("list");' >List</li>
+  
+          <li class="nav-link" id="addNew"
+          onclick='Book.showIt("add-section");  Book.toggleActive("addNew");' >Add new</li>
+  
+          <li class="nav-link" id="contact"
+          onclick='Book.showIt("about-section"); Book.toggleActive("contact");' >Contact</li>
+      </ul>
+    </nav>
+  
+    `;
+  }
+
+  static showTime() {
+    const dateTime = document.querySelector('.time');
+    const timeDate = () => {
+      setInterval(() => {
+        const date = new Date().toUTCString();
+        dateTime.innerHTML = date.toString().substring(0, date.indexOf(' GMT'));
+      }, 0);
+    };
+
+    timeDate();
+  }
 }
 
 window.onload = () => {
   Book.showBooks();
   document.querySelector('form').addEventListener('submit', Book.addNew);
+
+  Book.showNaviagtionBar();
+  Book.showTime();
 };
